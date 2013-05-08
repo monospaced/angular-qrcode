af69fe81b232f6b0e72dac875da8addb138c34ce
Scott Boyle <scott@monospaced.co.uk>
2013-05-08 21:02:24 +0100

    v0.0.0

---
+++ qrcode.js
@@ -0,0 +1,41 @@
+/*
+ * angular-qrcode v0.0.0
+ * (c) 2013 Monospaced http://monospaced.com
+ * License: MIT
+ */
+
+angular.module('monospaced.qrcode', [])
+  .directive('qrcode', function(){
+    return {
+      restrict: 'E',
+      link: function(scope, element, attr) {
+
+        var type = parseInt(attr.type, 10),
+            correction = attr.correction;
+
+        if (type > 10) {
+          type = 10;
+        }
+
+        if (type < 2) {
+          type = 2;
+        }
+
+        if (correction !== 'L' || correction !== 'M' || correction !== 'Q' || correction !== 'H') {
+          corretion = false;
+        }
+
+        var text = element.text().replace(/^\s+|\s+$/g, ''),
+            qrFactory = qrcode(type || '4', correction || 'M'),
+            qr;
+
+        qrFactory.addData(text);
+        qrFactory.make();
+
+        qr = qrFactory.createImgTag();
+
+        element.replaceWith(qr);
+
+      }
+    };
+  });
