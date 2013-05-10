f9d8116f5d1f21580720c19c43e6aafac6f8ccad
Scott Boyle <scott@monospaced.co.uk>
2013-05-10 22:47:51 +0100

    v0.0.2

--- qrcode.js
+++ qrcode.js
@@ -1,41 +1,80 @@
 /*
- * angular-qrcode v0.0.1
+ * angular-qrcode v0.0.2
  * (c) 2013 Monospaced http://monospaced.com
  * License: MIT
  */
 
 angular.module('monospaced.qrcode', [])
-  .directive('qrcode', function(){
+  .directive('qrcode', ['$timeout', '$window', function($timeout, $window){
+
+    var canvas2D = !!$window.CanvasRenderingContext2D,
+        levels = {
+          'L': 'Low',
+          'M': 'Medium',
+          'Q': 'Quartile',
+          'H': 'High'
+        },
+        draw = function(context, qr, modules, tile){
+          for (var row = 0; row < modules; row++) {
+            for (var col = 0; col < modules; col++) {
+              var w = (Math.ceil((col + 1) * tile) - Math.floor(col * tile)),
+                  h = (Math.ceil((row + 1) * tile) - Math.floor(row * tile));
+              context.fillStyle = qr.isDark(row, col) ? '#000' : '#fff';
+              context.fillRect(Math.round(col * tile), Math.round(row * tile), w, h);
+            }
+          }
+        };
+
     return {
       restrict: 'E',
-      link: function(scope, element, attr) {
+      template: '<canvas></canvas>',
+      transclude: true,
+      compile: function(element, attrs, transclude){
+        var domElement = element[0],
+            canvas = element.find('canvas')[0],
+            context = canvas.getContext('2d'),
+            version = Math.max(1, Math.min(parseInt(attrs.version, 10), 10)) || 4,
+            correction = attrs.errorCorrectionLevel in levels ? attrs.errorCorrectionLevel : 'M',
+            trim = /^\s+|\s+$/g,
+            qr = qrcode(version, correction);
 
-        var type = parseInt(attr.type, 10),
-            correction = attr.correction;
+        qr.make();
 
-        if (type > 10) {
-          type = 10;
+        var modules = qr.getModuleCount(),
+            size = parseInt(attrs.size, 10) || modules * 2,
+            tile = size / modules,
+            render = function(qr, text){
+              qr.addData(text);
+              qr.make();
+              if (canvas2D) {
+                draw(context, qr, modules, tile);
+              } else {
+                domElement.innerHTML = qr.createImgTag(tile, 0);
+              }
+            };
+
+        canvas.width = canvas.height = size;
+
+        if (!attrs.text) {
+
+          return function(scope, element, attrs){
+            transclude(scope, function(clone){
+              $timeout(function(){
+                var text = clone.text().replace(trim, '');
+                render(qr, text);
+              });
+            });
+          };
+
+        } else {
+
+          attrs.$observe('text', function(value){
+            var text = value.replace(trim, ''),
+                qr = qrcode(version, correction);
+            render(qr, text);
+          });
+
         }
-
-        if (type < 2) {
-          type = 2;
-        }
-
-        if (correction !== 'L' || correction !== 'M' || correction !== 'Q' || correction !== 'H') {
-          corretion = false;
-        }
-
-        var text = element.text().replace(/^\s+|\s+$/g, ''),
-            qrFactory = qrcode(type || '4', correction || 'M'),
-            qr;
-
-        qrFactory.addData(text);
-        qrFactory.make();
-
-        qr = qrFactory.createImgTag();
-
-        element.replaceWith(qr);
-
       }
     };
-  });
+  }]);
