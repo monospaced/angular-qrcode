29d8c3729aa554b4f73cc146b92aff3a83c97c99
Scott Boyle <scott@monospaced.com>
2014-01-30 11:22:19 +0000

    handle capcity errors #5

--- README.md
+++ README.md
@@ -33,6 +33,8 @@ Permitted values
 
 * size: integer
 
+The amount of data (measured in bits) must be within capacity according to the selected version and error correction level, see http://www.qrcode.com/en/about/version.html.
+
 Install
 -------
 
--- qrcode.js
+++ qrcode.js
@@ -1,11 +1,11 @@
 /*
- * angular-qrcode v3.0.0
+ * angular-qrcode v3.1.0
  * (c) 2013 Monospaced http://monospaced.com
  * License: MIT
  */
 
 angular.module('monospaced.qrcode', [])
-  .directive('qrcode', ['$timeout', '$window', function($timeout, $window){
+  .directive('qrcode', ['$window', function($window) {
 
     var canvas2D = !!$window.CanvasRenderingContext2D,
         levels = {
@@ -14,13 +14,15 @@ angular.module('monospaced.qrcode', [])
           'Q': 'Quartile',
           'H': 'High'
         },
-        draw = function(context, qr, modules, tile){
+        draw = function(context, qr, modules, tile) {
           for (var row = 0; row < modules; row++) {
             for (var col = 0; col < modules; col++) {
               var w = (Math.ceil((col + 1) * tile) - Math.floor(col * tile)),
                   h = (Math.ceil((row + 1) * tile) - Math.floor(row * tile));
+
               context.fillStyle = qr.isDark(row, col) ? '#000' : '#fff';
-              context.fillRect(Math.round(col * tile), Math.round(row * tile), w, h);
+              context.fillRect(Math.round(col * tile),
+                               Math.round(row * tile), w, h);
             }
           }
         };
@@ -28,22 +30,20 @@ angular.module('monospaced.qrcode', [])
     return {
       restrict: 'E',
       template: '<canvas></canvas>',
-      link: function(scope, element, attrs){
-
+      link: function(scope, element, attrs) {
         var domElement = element[0],
             canvas = element.find('canvas')[0],
             context = canvas2D ? canvas.getContext('2d') : null,
             trim = /^\s+|\s+$/g,
-            rendered = false,
+            error,
             version,
             errorCorrectionLevel,
             data,
             size,
             modules,
             tile,
-            qr;
-
-        var setVersion = function(value) {
+            qr,
+            setVersion = function(value) {
               version = Math.max(1, Math.min(parseInt(value, 10), 10)) || 4;
             },
             setErrorCorrectionLevel = function(value) {
@@ -53,10 +53,19 @@ angular.module('monospaced.qrcode', [])
               if (!value) {
                 return;
               }
+
               data = value.replace(trim, '');
               qr = qrcode(version, errorCorrectionLevel);
               qr.addData(data);
-              qr.make();
+
+              try {
+                qr.make();
+              } catch(e) {
+                error = e.message;
+                return;
+              }
+
+              error = false;
               modules = qr.getModuleCount();
             },
             setSize = function(value) {
@@ -65,12 +74,24 @@ angular.module('monospaced.qrcode', [])
               canvas.width = canvas.height = size;
             },
             render = function() {
+              if (!qr) {
+                return;
+              }
+
+              if (error) {
+                if (!canvas2D) {
+                  domElement.innerHTML = '<img src width="' + size + '"' +
+                                         'height="' + size + '">';
+                }
+                scope.$emit('qrcode:error', error);
+                return;
+              }
+
               if (canvas2D) {
                 draw(context, qr, modules, tile);
               } else {
                 domElement.innerHTML = qr.createImgTag(tile, 0);
               }
-              rendered = true;
             };
 
         setVersion(attrs.version);
@@ -81,6 +102,7 @@ angular.module('monospaced.qrcode', [])
           if (!value) {
             return;
           }
+
           setVersion(value);
           setData(data);
           setSize(size);
@@ -91,6 +113,7 @@ angular.module('monospaced.qrcode', [])
           if (!value) {
             return;
           }
+
           setErrorCorrectionLevel(value);
           setData(data);
           setSize(size);
@@ -101,6 +124,7 @@ angular.module('monospaced.qrcode', [])
           if (!value) {
             return;
           }
+
           setData(value);
           setSize(size);
           render();
@@ -110,10 +134,10 @@ angular.module('monospaced.qrcode', [])
           if (!value) {
             return;
           }
+
           setSize(value);
           render();
         });
-
       }
     };
   }]);
